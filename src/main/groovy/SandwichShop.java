9ff59dbeb90613b22b2616e862274ec3e2d558b4
jesicamatul <evelinmtlj@gmail.com>
2025-09-26 18:41:52 -0400

    final output

--- src/main/groovy/SandwichShop.java
+++ src/main/groovy/SandwichShop.java
@@ -6,38 +6,29 @@ public class SandwichShop {
         System.out.print("Enter the size of your sandwich 1 or 2:"); // ask user sandwich size
         int size = scanner.nextInt();
         double price1 = 5.45;
-        double price2  = 8.95;
-        double totalPrice= 0;
+        double price2 = 8.95;
+        double totalPrice = 0;
         if (size == 1) {
-            System.out.println("Your price is 5.45");
+            totalPrice = price1;
         } else if (size == 2) {
-            System.out.println("Your price is 8.95");
-        }else {
+            totalPrice = price2;
+        } else {
             System.out.println("Invalid sandwich size");
+        }
 
-System.out.println("Enter your age:");
+
+        System.out.print("Enter your age:");
         int age = scanner.nextInt();
 
-        if (age<=17) {
-
-
-        } else if (age <= 65) {
-
+        if (age <= 17) {
+            totalPrice = totalPrice - (totalPrice * .10);
+        } else if (age >= 65) {
+            totalPrice = totalPrice - (totalPrice * .20);
         }
-        System.out.print("The cost of you sandwich is $%.2f%n", price);
+
+
+        System.out.printf("The cost of your sandwich is $%.2f%n", totalPrice);
     }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
