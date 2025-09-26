5e8ddef576c854b5370349e40faa922bee5732b8
jesicamatul <evelinmtlj@gmail.com>
2025-09-26 15:36:56 -0400

    users output for sandwich

--- src/main/groovy/SandwichShop.java
+++ src/main/groovy/SandwichShop.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
 public class SandwichShop {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the size of your sandwich 1 or 2:");
-        String  sizeSandwich = scanner.nextLine();
+        int sandwich = scanner.nextInt();
+        if (size == 1) {
+            System.out.println("Your price is 5.45");
+             {
+                price sandwich2 = 8.95;
+                System.out.println("Your price is ");
+             }
+
+
+        }
+
+        }
 
     }
 
-}
+
+
+
+
+
+
+
+
+
