34f37de712577c3ac2c99bcccc15befb3e4a1400
jesicamatul <evelinmtlj@gmail.com>
2025-09-26 16:16:14 -0400

    checking a way to output the price

--- src/main/groovy/SandwichShop.java
+++ src/main/groovy/SandwichShop.java
@@ -5,24 +5,26 @@ public class SandwichShop {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the size of your sandwich 1 or 2:"); // ask user sandwich size
         int size = scanner.nextInt();
-
+        double price1 = 5.45;
+        double price2  = 8.95;
+        double totalPrice= 0;
         if (size == 1) {
             System.out.println("Your price is 5.45");
         } else if (size == 2) {
             System.out.println("Your price is 8.95");
-
-
-        }
+        }else {
+            System.out.println("Invalid sandwich size");
 
 System.out.println("Enter your age:");
         int age = scanner.nextInt();
 
         if (age<=17) {
-            System.out.print("You get a 10% discount");
+
+
         } else if (age <= 65) {
-            System.out.print("You receive a 20% discount");
 
         }
+        System.out.print("The cost of you sandwich is $%.2f%n", price);
     }
 }
 
