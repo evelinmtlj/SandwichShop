5c19a3070847a345bfd2e42f347dde76f047a0ed
jesicamatul <evelinmtlj@gmail.com>
2025-09-26 14:47:05 -0400

    asking user sandwich size

---
+++ src/main/groovy/SandwichShop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of your sandwich 1 or 2:");
+        String  sizeSandwich = scanner.nextLine();
+
+    }
+
+}
